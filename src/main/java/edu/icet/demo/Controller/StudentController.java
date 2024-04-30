0092f529e225cb31445821dbc4ec9787f0898e02
Huzaifa Ameer <113491165+huzaifameer@users.noreply.github.com>
2024-04-30 15:10:50 +0530

    commit-4

---
+++ src/main/java/edu/icet/demo/Controller/StudentController.java
@@ -0,0 +1,26 @@
+package edu.icet.demo.Controller;
+
+import edu.icet.demo.Student;
+import edu.icet.demo.StudentService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+    @GetMapping("/student")
+    public String getStudent() {
+        StudentService service = new StudentService();
+        Student student = service.getStudent();
+        System.out.println(student);
+        return "Frank";
+    }
+
+
+    @GetMapping("/teacher")
+    public String getTeacher() {
+        StudentService service = new StudentService();
+        Student student = service.getStudent();
+        System.out.println(student);
+        return "ICET";
+    }
+}
